f28f12c81a82c98cd763a73de40ca6a6589a96b3
ily19 <ouakouak.ilyes@gmail.com>
2026-05-05 18:23:09 +0200

    Time: 3 ms (21.7%), Space: 12.5 MB (48.91%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -23,3 +23,50 @@ class Solution(object):
         currentNode.next = list1 or list2
 
         return dummy.next
+    
+
+"""
+    dummy = ListNode(-1)
+    current = dummy
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            current.next = list1
+            list1 = list1.next
+        else:
+            current.next = list2
+            list2 = list2.next
+        current = current.next
+
+    
+    current.next = list1 if list1 else list2
+    
+    # this line is just saying this 
+    # Is list1 NOT empty?
+    if list1:
+        current.next = list1  # Hook the ENTIRE remaining chain of list1 here.
+    # Otherwise (if list1 is empty), it must be list2 that has stuff left.
+    else:
+        current.next = list2  # Hook the ENTIRE remaining chain of list2 here.
+    
+
+    return dummy.next
+
+ """
+
+"""
+    State:  [Dummy] -> [Node A]
+                ^
+            current is here
+
+    Action: current.next = Node B
+            current = current.next
+    Result: [Dummy] -> [Node A] -> [Node B]
+                            ^
+                        current is here!
+
+    Action: current = current.next
+    Result: [Dummy] -> [Node A] -> [Node B]
+                                        ^
+                                    Current is NOW here. Ready for Node C.
+"""
