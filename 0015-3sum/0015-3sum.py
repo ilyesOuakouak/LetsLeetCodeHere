25d1cef2fc21f54ce9f1044b474b9ecfcb803afd
ily19 <ouakouak.ilyes@gmail.com>
2026-05-05 15:42:06 +0200

    Time: 747 ms (68.77%), Space: 18.3 MB (51.35%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -27,4 +27,50 @@ class Solution:
                             r -= 1
         return result
 
+        """ res = []
+        # TIME: O(N log N) - Sorting is fast, but not the bottleneck
+        nums.sort()  # 1. Sort to enable Two Pointers logic
+        
+        n = len(nums)
+        
+        # TIME: This outer loop runs N times
+        for i in range(n):
+            # Optimization: If the current number is positive, we can't form 0 
+            # because the array is sorted (all following numbers are also positive)
+            if nums[i] > 0:
+                break
+                
+            # DUPLICATE TRAP 1: Skip the same anchor number to avoid duplicate triplets
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            
+            # 2. Set up Two Pointers
+            l, r = i + 1, n - 1
+            
+            # TIME: This inner while loop runs roughly N times for EACH 'i'
+            # Total = N * N = O(N^2)
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+                
+                if total < 0:
+                    l += 1
+                elif total > 0:
+                    r -= 1
+                else:
+                    # Found a triplet!
+                    res.append([nums[i], nums[l], nums[r]])
+                    
+                    # DUPLICATE TRAP 2: We need to move pointers AND skip duplicates
+                    l += 1
+                    r -= 1
+                    
+                    # Skip identical 'left' values
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                        
+                    # (Optional optimization: skip identical 'right' values, 
+                    # but just skipping left is enough to avoid duplicates)
+                    
+        return res """
+
             
