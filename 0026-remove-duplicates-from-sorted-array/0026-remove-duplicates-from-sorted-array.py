02203a1620e3f705fbd8dde7c23f5af821f1a806
ily19 <ouakouak.ilyes@gmail.com>
2026-05-06 17:15:12 +0200

    Time: 4 ms (41.81%), Space: 13.8 MB (71.18%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,5 +1,8 @@
 class Solution(object):
     def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+            
         l = 0
         for r in range(1, len(nums)):
             if nums[r] != nums[l]:
