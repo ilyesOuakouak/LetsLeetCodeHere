58800bdc9a15078abf725be60ebb9ea7f5074392
ily19 <ouakouak.ilyes@gmail.com>
2026-05-08 14:05:41 +0200

    Time: 0 ms (100%), Space: 12.4 MB (57.23%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -13,10 +13,52 @@ class Solution(object):
 
         if not digits:
             return []
+
+        res = []
+
+        
+        def backtrack(index, path):
+            # 1. Base Case: If the path is long enough...
+            # Which means we reached a leaf 
+            if index == len(digits):
+                res.append(path)
+                return 
+            
+            # get letter of the current digit 
+            letters = mapping[digits[index]]
+
+            for letter in letters:
+                backtrack(index + 1, path + letter)
+
+
+        
+
+
+        # Start the recursion
+        backtrack(0, "")
+        return res
+
+
+
+
+
+        """ This is a BFS solution 
+        mapping = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        if not digits:
+            return []
         # 23
 
-        queue = deque([""])
-        result = []
+        queue = deque([""]
 
         for digit in digits:
             size = len(queue)
@@ -29,7 +71,7 @@ class Solution(object):
         
         return list(queue)
 
-
+        """
 
 
         
