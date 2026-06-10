b1f12c0226d3a055be93ba29090efc1d727ca16e
ily19 <ouakouak.ilyes@gmail.com>
2026-06-10 16:27:20 +0200

    Time: 4 ms (48.64%), Space: 13.2 MB (35.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,63 @@
 class Solution:
     def twoSum(self, nums, target):   
         dic = {}
+     
+
+        for i in range(len(nums)):
+            remaining = target - nums[i]
+
+            if remaining in dic:
+                return [i, dic[remaining]]
+            
+            if nums[i] not in dic:
+                dic[nums[i]] = i
+            
+            
+            
+        return []
+
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+dic = {}
 
         for i in range(len(nums)):
             result = target - nums[i]
@@ -11,8 +68,4 @@ class Solution:
 
         return []
 
-
-
-        
-
-
+"""
