4592c079ad6745fd0166ff82bcb8375a5abf27a6
ily19 <ouakouak.ilyes@gmail.com>
2026-06-12 14:40:14 +0200

    Time: 1 ms (86.04%), Space: 12.5 MB (37.51%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,48 +1,28 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        backets = {
-            '(': ')',
-            '{': '}',
-            '[': ']'
-        }
-
+        brackets = { "(": ")", "[": "]", "{":"}" }
         stack = []
-        if len(s) == 1:
-            return False
-            
+
         for c in s:
-            
-            if c in backets:
+            if c in brackets:
                 stack.append(c)
+        
             else:
-                if stack:
-                    poped_element = stack.pop()
-
-                    if c != backets[poped_element]:
-                        return False
-                else: return False
                 
-        return not stack
+                if not stack:
+                    return False
+                
+                poped_char = stack.pop()
+                if brackets[poped_char] != c:
+                    return False
 
-# Here is a simplified version - great 
+        return True if not stack else False
+
+
+
+
+""" 
+    dry-run
+    stack = '[('
 
 """
-stack = []
-bracket_map = {')': '(', ']': '[', '}': '{'}
-
-for char in s:
-    if char in bracket_map:
-        top = stack.pop() if stack else '#'
-
-        if top != bracket_map[char]:
-            return False
-    else:
-        stack.append(char)
-return not stack
-"""            
-         
-        
