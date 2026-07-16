6793985e9889bb50175acdde83b9ab8e45051653
ily19 <ouakouak.ilyes@gmail.com>
2026-07-16 16:51:45 +0200

    Time: 31 ms (68.08%), Space: 13.4 MB (28.84%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -13,9 +13,8 @@ class Solution(object):
         for i in range(last_index):
             if i > max_reach:
                 return False
-                
+
             max_reach = max(max_reach, i + nums[i])
-            
 
 
             if max_reach >= last_index:
@@ -23,3 +22,17 @@ class Solution(object):
             
         
         return False
+
+    
+    """
+    def canJump(self, nums):
+    max_reach = 0
+    last_index = len(nums) - 1
+
+    for i in range(len(nums)):
+        if i > max_reach:
+            return False
+        max_reach = max(max_reach, i + nums[i])
+
+    return True
+    """
