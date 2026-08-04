a366d9d3529ec8820eb657059d974d14edad4769
ily19 <ouakouak.ilyes@gmail.com>
2026-08-04 17:38:09 +0200

    Time: 0 ms (100%), Space: 12.4 MB (60.93%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        
+        dp = [[1] * n for _ in range(m)]
+
+        for r in range(1, m):
+            for c in range(1, n):
+                dp[r][c] = dp[r-1][c] + dp[r][c-1]
+        
+        return dp[m-1][n-1]
+
+
