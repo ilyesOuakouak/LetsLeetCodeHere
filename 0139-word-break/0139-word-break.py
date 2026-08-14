6df8df231a379533336d2567e398c803170ce084
ily19 <ouakouak.ilyes@gmail.com>
2026-08-14 16:37:38 +0200

    Time: 11 ms (17.6%), Space: 12.5 MB (15.74%) - LeetHub

---
+++ 0139-word-break/0139-word-break.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] == True and s[j:i] in wordDict:
+                    dp[i] = True
+        
+        return dp[len(s)]
+
+        
+        
